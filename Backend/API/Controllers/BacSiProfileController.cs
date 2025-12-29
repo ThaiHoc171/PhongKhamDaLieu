f1beb1b64847274812d110c0132f2efd38641321
PhuocKhang <kn01222192288@gmail.com>
2025-12-29 23:19:06 +0700

    Update BacSiProfile, PhongKham API

---
+++ Backend/API/Controllers/BacSiProfileController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using Domain.DTO;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BacSiProfileController : ControllerBase
+    {
+        private readonly BacSiProfileService _service;
+
+        public BacSiProfileController(BacSiProfileService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("DanhSachBacSiProfile")]
+        public IActionResult DanhSach()
+        {
+            return Ok(_service.DanhSachBacSiProfile());
+        }
+
+        [HttpGet("TimBacSiProfileTheoID")]
+        public IActionResult TimTheoID(int bacSiProfileID)
+        {
+            return Ok(_service.LayBacSiProfileByID(bacSiProfileID));
+        }
+
+        [HttpGet("TimBacSiProfileTheoNhanVien")]
+        public IActionResult TimTheoNhanVien(int nhanVienID)
+        {
+            return Ok(_service.LayBacSiProfileByNhanVienID(nhanVienID));
+        }
+
+        [HttpPost("ThemBacSiProfile")]
+        public IActionResult Them([FromBody] ThemBacSiProfileDTO bacSi)
+        {
+            if (_service.ThemBacSiProfile(bacSi))
+                return Ok("Thêm hồ sơ bác sĩ thành công.");
+
+            return BadRequest("Thêm hồ sơ bác sĩ thất bại.");
+        }
+
+        [HttpPut("CapNhatBacSiProfile")]
+        public IActionResult CapNhat([FromBody] BacSiProfileDTO bacSi)
+        {
+            if (_service.CapNhatBacSiProfile(bacSi))
+                return Ok("Cập nhật hồ sơ bác sĩ thành công.");
+
+            return BadRequest("Cập nhật hồ sơ bác sĩ thất bại.");
+        }
+    }
+}
---
+++ Backend/API/Controllers/PhongKhamController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using Domain.DTO;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PhongKhamController : ControllerBase
+    {
+        private readonly PhongKhamService _service;
+
+        public PhongKhamController(PhongKhamService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("DanhSachPhongKham")]
+        public IActionResult DanhSach()
+        {
+            return Ok(_service.DanhSachPhongKham());
+        }
+
+
+        [HttpPut("CapNhatPhongKham")]
+        public IActionResult CapNhat([FromBody] PhongKhamDTO phongKham)
+        {
+            if (_service.CapNhatPhongKham(phongKham))
+                return Ok("Cập nhật phòng khám thành công.");
+
+            return BadRequest("Cập nhật phòng khám thất bại.");
+        }
+    }
+}
