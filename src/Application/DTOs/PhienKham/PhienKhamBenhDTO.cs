c751a27487a09cfab6c0756fe84b1bfaff33b07c
Dow <thaihocng204@gmail.com>
2026-01-18 11:58:23 +0700

    PhienKham

---
+++ src/Application/DTOs/PhienKham/PhienKhamBenhDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs;
+
+public class PhienKhamBenhRequestDTO
+{
+	public int PhienKhamID { get; set; }
+	public int LoaiBenhID { get; set; }
+	public string LoaiChuanDoan { get; set; } = default!;
+	public string? GhiChu { get; set; }
+}
---
+++ src/Application/Interfaces/PhienKham/IPhienKhamRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
+
+public interface IPhienKhamRepository
+{
+	Task<PhienKham?> GetByIdAsync(int id);
+	Task<int> AddAsync(PhienKham phienKham);
+	Task UpdateAsync(PhienKham phienKham);
+}
+
