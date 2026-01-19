2ecfefb085e2c089f0a90a78d93427647f45d06d
Dow <thaihocng204@gmail.com>
2026-01-19 20:17:34 +0700

    TaoLich

---
+++ src/Domain/Entities/LichLamViec.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class LichLamViec
+{
+	public int LichLamViecID { get; set; }
+	public int NhanVienID { get; set; }
+	public DateTime Ngay { get; set; }
+	public int CaLamViec { get; set; }
+	public string? GhiChu { get; set; }
+
+	//Constructor Tạo
+	public LichLamViec(int nhanVienID, DateTime ngay, int caLamViec, string? ghiChu, bool IsNgayNghi)
+	{
+		if (ngay.Date < DateTime.Now.Date)
+		{
+			throw new ArgumentException("Ngày làm việc không được là ngày trong quá khứ.");
+		}
+		if (IsNgayNghi)
+		{
+			throw new ArgumentException("Ngày làm việc không được trùng với ngày nghỉ đã được thiết lập.");
+		}
+		if (caLamViec < 1 || caLamViec > 2)
+		{
+			throw new ArgumentException("Ca làm việc không hợp lệ.");
+		}
+		NhanVienID = nhanVienID;
+		Ngay = ngay;
+		CaLamViec = caLamViec;
+		GhiChu = ghiChu;
+	}
+	// Constructor Map dữ liệu từ DB
+	public LichLamViec(int lichLamViecID, int nhanVienID,  DateTime ngay, int caLamViec, string? ghiChu)
+	{
+		LichLamViecID = lichLamViecID;
+		NhanVienID = nhanVienID;
+		Ngay = ngay;
+		CaLamViec = caLamViec;
+		GhiChu = ghiChu;
+	}
+}
---
+++ src/Infrastructure/Repository/LichLamViecRepository.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using Domain.Entities;
+using Application.Interfaces;
+
+namespace Infrastructure.Repository;
+
+public class LichLamViecRepository : ILichLamViecRepository
+{
+	private readonly string _connectionString;
+	private SqlConnection? _conn;
+	private SqlTransaction? _tran;
+	public LichLamViecRepository(IConfiguration config)
+	{
+		_connectionString = config.GetConnectionString("DefaultConnection")
+			?? throw new ArgumentNullException("Connection string not found");
+	}
+	public async Task AddAsync(LichLamViec entity)
+	{
+		const string sql = @"
+			INSERT INTO LichLamViecNhanVien
+			(NhanVienID, Ngay, CaLamViec, GhiChu)
+			VALUES (@NhanVienID, @Ngay, @Ca, @GhiChu)
+		";
+
+		await using var cmd = new SqlCommand(sql, _conn!, _tran);
+		cmd.Parameters.AddWithValue("@NhanVienID", entity.NhanVienID);
+		cmd.Parameters.AddWithValue("@Ngay", entity.Ngay);
+		cmd.Parameters.AddWithValue("@Ca", entity.CaLamViec);
+		cmd.Parameters.AddWithValue("@GhiChu", (object?)entity.GhiChu ?? DBNull.Value);
+
+		await cmd.ExecuteNonQueryAsync();
+	}
+	public async Task<bool> IsExitsAsync(int nhanVienID, DateTime ngay, int caLamViec)
+	{
+		const string sql = @"
+		SELECT 1 
+		FROM LichLamViecNhanVien
+		WHERE NhanVienID = @NhanVienID
+		  AND Ngay = @Ngay
+		  AND CaLamViec = @CaLamViec";
+
+		await using var cmd = new SqlCommand(sql, _conn!, _tran);
+		cmd.Parameters.AddWithValue("@NhanVienID", nhanVienID);
+		cmd.Parameters.AddWithValue("@Ngay", ngay.Date);
+		cmd.Parameters.AddWithValue("@CaLamViec", caLamViec);
+
+		var result = await cmd.ExecuteScalarAsync();
+		return result != null;
+	}
+
+	public async Task<bool> IsNgayNghiAsync(DateTime ngay, int nhanVienID)
+	{
+		const string sql = @"
+		SELECT 1 
+		FROM NgayNghiNhanVien
+		WHERE Ngay = @Ngay
+		  AND NhanVienID = @NhanVienID";
+
+		await using var cmd = new SqlCommand(sql, _conn!, _tran);
+		cmd.Parameters.AddWithValue("@Ngay", ngay.Date);
+		cmd.Parameters.AddWithValue("@NhanVienID", nhanVienID);
+
+		var result = await cmd.ExecuteScalarAsync();
+		return result != null;
+	}
+
+	public async Task BeginTransactionAsync()
+	{
+		_conn = new SqlConnection(_connectionString);
+		await _conn.OpenAsync();
+		_tran = _conn.BeginTransaction();
+	}
+
+	public async Task CommitAsync()
+	{
+		await _tran!.CommitAsync();
+		await _conn!.CloseAsync();
+	}
+
+	public async Task RollbackAsync()
+	{
+		await _tran!.RollbackAsync();
+		await _conn!.CloseAsync();
+	}
+}
