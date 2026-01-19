98231e21f8d9411bfc24ae212f88d5af15671832
NHPKhang <khangnguyen.agu@gmail.com>
2026-01-19 13:59:42 +0700

    NHPKhang_Create_CaKhamEntites

--- src/Domain/Entities/CaKham.cs
+++ src/Domain/Entities/CaKham.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Domain.Entities;
 
-namespace Domain.Entities
+public class CaKham
 {
-    internal class CaKham
-    {
-    }
+    public int CaKhamID { get; private set; }
+    public int LichLamViecID { get; private set; }
+    public int PhongChucNangID { get; private set; }
+    public DateTime NgayKham { get; private set; }
+    public int KhungGioID { get; private set; }
+    public int BenhNhanID { get; private set; }
+    public string? LyDoKham { get; private set; }
+    public string TrangThai { get; private set; }
+    public DateTime NgayDat { get; private set; }
+    public string? GhiChu { get; private set; }
 }
