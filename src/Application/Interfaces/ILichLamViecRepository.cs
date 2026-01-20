065730e22ae7940acabc1d0daf5e1475dbbf2301
Dow <thaihocng204@gmail.com>
2026-01-20 19:13:56 +0700

    Lichlamviec

--- src/Application/Interfaces/ILichLamViecRepository.cs
+++ src/Application/Interfaces/ILichLamViecRepository.cs
@@ -8,6 +8,8 @@ public interface ILichLamViecRepository
 	Task <bool> IsExitsAsync(int nhanVienID, DateTime ngay, int caLamViec);
 	Task<bool> IsChucVuExitsAsync(int ChucVuID, DateTime ngay, int caLamViec);
 	Task<LichLamViec?> GetByIdAsync(int ID);
+	Task<List<LichLamViec>> GetAllAsync();
+	Task<List<LichLamViec>> GetByNhanVienIdTheoTuanAsync(int NhanVienID, DateTime tuNgay,DateTime denNgay);
 	Task AddAsync(LichLamViec lich);
 	Task BeginTransactionAsync();
 	Task CommitAsync();
