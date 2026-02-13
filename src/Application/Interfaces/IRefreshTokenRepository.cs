2483c1d66c41846a822a999632da8beb698b557f
NHPKhang <khangnguyen.agu@gmail.com>
2026-02-13 09:32:54 +0700

    Cấu hình Jwt

---
+++ src/Application/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
+public interface IRefreshTokenRepository
+{
+    Task SaveAsync(RefreshToken token);
+    Task<RefreshToken?> GetAsync(string tokenHash);
+    Task RevokeAsync(string tokenHash);
+    Task RevokeAllAsync(int taiKhoanId);
+}
+
