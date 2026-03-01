acd3eddefe44ccefc3b92ae31f133fab230ae0ee
Dow <thaihocng204@gmail.com>
2026-03-01 13:31:10 +0700

    fix benhnhan getall

--- src/Application/Interfaces/IBenhNhanRepository.cs
+++ src/Application/Interfaces/IBenhNhanRepository.cs
@@ -5,7 +5,7 @@ public interface IBenhNhanRepository
 {
 	Task<BenhNhan?> GetByIdAsync(int id);
 	Task<List<BenhNhan>> GetBenhNhans(string keyword);
-	Task<List<BenhNhan>> GetAllAsync();
+	Task<(List<BenhNhan> Data, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize);
 	Task<string?> GetNameByIdAsync(int id);
 	Task<int> GetForAuthAsync(int taiKhoanID);
 	Task<int> AddAsync(BenhNhan benhNhan);
