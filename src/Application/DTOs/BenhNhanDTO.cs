484ced5e30a98ab6af82feb4381b02f77b4199b2
Dow <thaihocng204@gmail.com>
2026-03-05 19:35:52 +0700

    fix something

--- src/Application/DTOs/BenhNhanDTO.cs
+++ src/Application/DTOs/BenhNhanDTO.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Application.DTOs;
 
-namespace Application.DTOs;
-
-public class ThemBenhNhanDTO
+public class BenhNhanRequestDTO
 {
-	public int? ThongTinID { get; set; } // nếu null thì tạo ThongTinCaNhan mới
+	public int? ThongTinID { get; set; }
 	public string? HoTen { get; set; }
 	public DateTime? NgaySinh { get; set; }
 	public string? GioiTinh { get; set; }
@@ -28,7 +22,22 @@ public class BenhNhanResponseDTO
 	public string? EmailLienHe { get; set; }
 	public string? GhiChu { get; set; }
 }
+public class BenhNhanIdResponseDTO
+{
+	public int BenhNhanID { get; set; }
+	public int ThongTinID { get; set; }	
+	public string? GhiChu { get; set; }
+	public string? HoTen { get; set; }
+	public DateTime? NgaySinh { get; set; }
+	public string? GioiTinh { get; set; }
+	public string? SDT { get; set; }
+	public string? EmailLienHe { get; set; }
+	public string? DiaChi { get; set; }
+	public string? Avatar { get; set; }
 
+	public DateTime? NgayTao { get; set; }
+	public DateTime? NgayCapNhat { get; set; }
+}
 public class CapNhatBenhNhanDTO
 {
 	public string? GhiChu { get; set; }
