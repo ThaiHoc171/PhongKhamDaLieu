c54c47a7e359ac94a3ad4301f6da5cfb0fcbc137
NHPKhang <khangnguyen.agu@gmail.com>
2026-03-06 18:04:09 +0700

    Update Mobile

---
+++ Mobile/ql_phongkham/lib/features/clinic/presentation/widgets/auth/auth_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:ql_phongkham/core/theme/app_pallete.dart';
+
+class AuthButton extends StatelessWidget {
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  const AuthButton({
+    super.key,
+    required this.buttonText,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            AppPallete.gradient1,
+            AppPallete.gradient2,
+            AppPallete.gradient3,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed, // 🔥 quan trọng
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(395, 55),
+          backgroundColor: AppPallete.transparentColor,
+          shadowColor: AppPallete.transparentColor,
+        ),
+        child: Text(
+          buttonText,
+          style: const TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
+            color: AppPallete.whiteColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
