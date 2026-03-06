6c7697d4de1d673032538f201d2ef6d54370242e
Dow <thaihocng204@gmail.com>
2026-03-06 11:16:52 +0700

    benhNhan

--- src/Application/Services/BenhNhanService.cs
+++ src/Application/Services/BenhNhanService.cs
@@ -3,6 +3,7 @@ using Application.Interfaces;
 using Domain.Entities;
 using Domain.Enums;
 using Microsoft.Extensions.Configuration;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Application.Services;
 
@@ -131,23 +132,16 @@ public class BenhNhanService
 
 	public async Task<List<BenhNhanResponseDTO>> SearchdAsync(string keyword)
 	{
-		var list = await _benhNhanRepo.GetBenhNhans(keyword);
-		var result = new List<BenhNhanResponseDTO>();
-
-		foreach (var bn in list)
+		var data = await _benhNhanRepo.GetBenhNhans(keyword);
+		var result = data.Select(bn => new BenhNhanResponseDTO
 		{
-			var thongTin = await _thongTinRepo.GetByIdAsync(bn.ThongTinID);
-
-			result.Add(new BenhNhanResponseDTO
-			{
-				BenhNhanID = bn.BenhNhanID,
-				ThongTinID = bn.ThongTinID,
-				HoTen = thongTin?.HoTen ?? "",
-				SDT = thongTin?.SDT ?? "",
-				EmailLienHe = thongTin?.EmailLienHe ?? "",
-				GhiChu = bn.GhiChu
-			});
-		}
+			BenhNhanID = bn.BenhNhanID,
+			ThongTinID = bn.ThongTinCaNhan?.ThongTinID ?? 0,
+			HoTen = bn.ThongTinCaNhan?.HoTen ?? "",
+			SDT = bn.ThongTinCaNhan?.SDT ?? "",
+			EmailLienHe = bn.ThongTinCaNhan?.EmailLienHe ?? "",
+			GhiChu = bn.GhiChu
+		}).ToList();
 
 		return result;
 	}
