98af53514393d42b697b499a7b3f87457771e156
NHPKhang <khangnguyen.agu@gmail.com>
2026-03-07 00:56:31 +0700

    Test

--- src/Application/Services/PhienKham/PhienKhamThietBiService.cs
+++ src/Application/Services/PhienKham/PhienKhamThietBiService.cs
@@ -18,12 +18,12 @@ public class PhienKhamThietBiService
 	}
 
 	// DANH SÁCH THEO PHIÊN KHÁM
-	public async Task<List<PhienKhamThietBiResponseDTO>> DanhSachTheoPhienKhamAsync(int phienKhamID)
-	{
-		var entities = await _repo.GetByPhienKhamAsync(phienKhamID);
-		return entities.Select(MapToResponse).ToList();
-	}
-	// LẤY THEO ID
+	//public async Task<List<PhienKhamThietBiResponseDTO>> DanhSachTheoPhienKhamAsync(int phienKhamID)
+	//{
+	//	var entities = await _repo.GetByPhienKhamAsync(phienKhamID);
+	//	return entities.Select(MapToResponse).ToList();
+	//}
+	// LẤY THEO ID		
 	public async Task<PhienKhamThietBiResponseDTO?> LayTheoIdAsync(int id)
 	{
 		var entity = await _repo.GetByIdAsync(id);
