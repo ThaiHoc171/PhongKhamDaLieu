8e692ef220b7efc2bdfb1252bfff76ec5ec37c1e
Dow <thaihocng204@gmail.com>
2026-03-06 15:42:43 +0700

    fix PhienKham

--- src/API/Controllers/PhienKham/PhienKhamController.cs
+++ src/API/Controllers/PhienKham/PhienKhamController.cs
@@ -40,30 +40,29 @@ public class PhienKhamController : ControllerBase
 		await _service.KetThucAsync(id, chanDoanCuoi);
 		return Ok(new { message = "Kết thúc phiên khám thành công." });
 	}
-
 	[Authorize(Roles = "Admin")]
+	[HttpGet("benhnhan/{benhNhanId}")]
+	public async Task<IActionResult> LayTheoBenhNhan(int benhNhanId,[FromQuery] int pageNumber = 1,[FromQuery] int pageSize = 10)
+	{
+		var result = await _service.GetByBenhNhanAsync(benhNhanId, pageNumber, pageSize);
+		return Ok(result);
+	}
+	[Authorize(Policy = "BacSiOnly")]
 	[HttpGet]
-	public async Task<IActionResult> GetAll()
-	{
-		return Ok(await _service.LayTatCaAsync());
-	}
+	public async Task<IActionResult> LayDanhSach([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 15, 
+												 [FromQuery] int? nhanVienID = null, [FromQuery] string? trangThai = null)
+		=> Ok(await _service.GetPagedAsync(pageNumber, pageSize,nhanVienID,trangThai));
 
-	[Authorize(Roles = "Admin")]
-	[HttpGet("filter")]
-	public async Task<IActionResult> Filter(
-		[FromQuery] DateTime? tuNgay,
-		[FromQuery] DateTime? denNgay,
-		[FromQuery] string? trangThai,
-		[FromQuery] int? nhanVienID)
-	{
-		return Ok(await _service.LocAsync(tuNgay, denNgay, trangThai, nhanVienID));
-	}
+	[Authorize(Policy = "BacSiOnly")]
+	[HttpGet("timkiem")]
+	public async Task<IActionResult> Search([FromQuery] string keyword,[FromQuery] int? nhanVienID)
+		=> Ok(await _service.SearchAsync(keyword,nhanVienID));
 
 	[Authorize(Policy = "BacSiOnly")]
 	[HttpGet("{id}")]
 	public async Task<IActionResult> GetById(int id)
 	{
-		var result = await _service.LayTheoIdAsync(id);
+		var result = await _service.GetByIdAsync(id);
 		return result == null
 			? NotFound("Phiên khám không tồn tại")
 			: Ok(result);
