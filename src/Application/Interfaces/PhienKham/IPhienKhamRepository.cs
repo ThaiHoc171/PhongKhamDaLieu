984e5455b2fa202067b1579fd324104bb7a1956b
Dow <thaihocng204@gmail.com>
2026-03-07 00:02:45 +0700

    big update PhienKham _readmodel

--- src/Application/Interfaces/PhienKham/IPhienKhamRepository.cs
+++ src/Application/Interfaces/PhienKham/IPhienKhamRepository.cs
@@ -1,14 +1,14 @@
-﻿using Domain.Entities;
-namespace Application.Interfaces;
+﻿using Application.DTOs;
+using Domain.Entities;
 public interface IPhienKhamRepository
 {
 	Task<PhienKham?> GetByIdAsync(int id);
+	Task<int> AddAsync(PhienKham entity);
+	Task UpdateAsync(PhienKham entity);
+	Task KetThucAsync(PhienKham entity);
 	Task<int?> GetBenhNhanIdByPhienKhamIdAsync(int phienKhamID);
-	Task<(List<PhienKham> Data, int TotalCount)> GetByBenhNhanPagedAsync(int benhNhanID, int pageNumber, int pageSize);
-	Task<int> AddAsync(PhienKham phienKham);
-	Task UpdateAsync(PhienKham phienKham);
-	Task KetThucAsync(PhienKham phienKham);
-	Task<(List<PhienKham> Data, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize, int? nhanVienID, string? trangThai);
-	Task<List<PhienKham>> SearchAsync(string keyword, int? nhanVienID);
+	Task<(List<PhienKhamResponseLiteDTO>, int)>	GetPagedAsync(int page, int size, int? nhanVienID, string? trangThai);
+	Task<(List<PhienKhamResponseLiteDTO>, int)>	GetByBenhNhanPagedAsync(int benhNhanID, int page, int size);
+	Task<List<PhienKhamResponseLiteDTO>> SearchAsync(string keyword, int? nhanVienID);
+	Task<PhienKhamResponseDTO?> GetDetailAsync(int id);
 }
-
