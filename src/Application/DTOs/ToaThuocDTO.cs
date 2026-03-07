2c5e6406e207f1fdc23939426f41c1e8abf6b57c
Dow <thaihocng204@gmail.com>
2026-03-07 15:18:59 +0700

    ToaThuoc

---
+++ src/Application/DTOs/ToaThuocDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace Application.DTOs;
+public class ToaThuocRequestDTO
+{
+	public int PhienKhamID { get; set; }
+	public int NhanVienKeDonID { get; set; }
+	public string? GhiChu { get; set; }
+	public List<ChiTietToaThuocRequestDTO> Thuoc { get; set; } = new();
+}
+
+public class ChiTietToaThuocRequestDTO
+{
+	public int ThuocID { get; set; }
+	public string? LieuDung { get; set; }
+	public int SoLuong { get; set; }
+}
+public class ToaThuocReadModel
+{
+	public int ToaThuocID { get; set; }
+	public DateTime NgayLap { get; set; }
+	public string? NguoiLap { get; set; }
+	public string? GhiChu { get; set; }
+}
+
+public class ChiTietToaThuocReadModel
+{
+	public string? TenThuoc { get; set; }
+	public string? LieuDung { get; set; }
+	public int SoLuong { get; set; }
+}
+
+
