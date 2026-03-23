ff4ecb824c5a2f7bafead9ff29607967617ce418
Dow <thaihocng204@gmail.com>
2026-03-23 13:10:33 +0700

    fix quyenChucVu

--- src/Infrastructure/Repository/QuyenRepository.cs
+++ src/Infrastructure/Repository/QuyenRepository.cs
@@ -10,23 +10,24 @@ public class QuyenRepository : IQuyenRepository
 	{
 		_connectionString = config.GetConnectionString("DefaultConnection")!;
 	}
-	public async Task<List<NameResponseDTO>> GetAllAsync()
+	public async Task<List<QuyenReadModel>> GetAllAsync()
 	{
 		const string sql = @"
-            SELECT QuyenID, TenQuyen
+            SELECT QuyenID, TenQuyen, Module
             FROM Quyen
         ";
-		var list = new List<NameResponseDTO>();
+		var list = new List<QuyenReadModel>();
 		await using var conn = new SqlConnection(_connectionString);
 		await using var cmd = new SqlCommand(sql, conn);
 		await conn.OpenAsync();
 		await using var reader = await cmd.ExecuteReaderAsync();
 		while (await reader.ReadAsync())
 		{
-			list.Add(new NameResponseDTO
+			list.Add(new QuyenReadModel
 			{
-				Id = reader.GetInt32(0),
-				Name = reader.GetString(1)
+				QuyenID = reader.GetInt32(0),
+				TenQuyen = reader.GetString(1),
+				Module = reader.GetString(2)
 			});
 		}
 		return list;
