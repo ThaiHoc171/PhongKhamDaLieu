01c96148ce0da510ade2f0aed81a238475f2b316
Dow <thaihocng204@gmail.com>
2026-03-27 09:31:03 +0700

    fix chucvu

--- src/Application/DTOs/ChucVuDTO.cs
+++ src/Application/DTOs/ChucVuDTO.cs
@@ -2,16 +2,17 @@
 public class ChucVuRequest
 {
     public string TenChucVu { get; set; } = "";
-    public string? MoTa { get; set; }
+    public string MoTa { get; set; } = "";
 	public string TrangThai { get; set; } = "";
 }
 public class ChucVuReadModel
 {
     public int ChucVuID { get; set; }
     public string TenChucVu { get; set; } = "";
-    public string? MoTa { get; set; }
-    public DateTime NgayTao { get; set; }
-    public string TrangThai { get; set; } = "";
+    public string MoTa { get; set; } = "";
+	public DateTime NgayTao { get; set; }
+	public DateTime? NgayCapNhat { get; set; }
+	public string TrangThai { get; set; } = "";
 }
 public class ChucVuListReadModel
 {
